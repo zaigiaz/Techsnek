cac58297a666cd7a93cf4850da6093520760d433
zaigiaz <shayne.tman@gmail.com>
2026-01-05 19:05:04 -0600

    going through tutorial

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
-
+import pygame
 
 # create basic game loop that displays grid
+ 
 
-def main():
-    print("hello")
+# game loop:
+# init(), processinput(), update(), render(), and run()
 
+pygame.init()
+window = pygame.display.set_mode((640,480))
 
+x = 120
+y = 120
+running = True
 
+while running:
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            break
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                break
+            elif event.key == pygame.K_RIGHT:
+                x += 8
+            elif event.key == pygame.K_LEFT:
+                x -= 8
+            elif event.key == pygame.K_DOWN:
+                y += 8
+            elif event.key == pygame.K_UP:
+                y -= 8
+
+    window.fill((0,0,0))
+    pygame.draw.rect(window,(0,0,255),(x,y,400,240))
+    pygame.display.update()
+
+pygame.quit()
